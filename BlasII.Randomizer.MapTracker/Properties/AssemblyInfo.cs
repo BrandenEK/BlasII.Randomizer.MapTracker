0d9c37a4787026b6620440071d178b198bbce4db
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-02 10:27:33 -0400

    core: update project info

--- BlasII.Randomizer.MapTracker/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer.MapTracker/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer.MapTracker;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.MapTracker.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonAdditionalDependencies("Modding API", "0.8.3", "Randomizer", "1.0.0")]
+[assembly: MelonAdditionalDependencies("Modding API", "1.0.3", "Randomizer", "1.0.4")]

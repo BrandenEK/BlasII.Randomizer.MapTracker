0a2b731dfd68ba7e4f3b76b4671075ecb5e9ec90
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-11-02 10:55:04 -0400

    core: import UI Framework

--- BlasII.Randomizer.MapTracker/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer.MapTracker/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer.MapTracker;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.MapTracker.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonAdditionalDependencies("Modding API", "1.0.3", "Randomizer", "1.0.4")]
+[assembly: MelonAdditionalDependencies("Modding API", "1.0.3", "UI Framework", "0.1.0", "Randomizer", "1.0.4")]

661a526b1cbedb4587863442b057fa0deea8e2bd
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-16 13:41:49 -0400

    core: change Logic enum

--- BlasII.Randomizer.MapTracker/Colors.cs
+++ BlasII.Randomizer.MapTracker/Colors.cs
@@ -8,9 +8,9 @@ internal static class Colors
 {
     public static Dictionary<Logic, Color> LogicColors { get; } = new()
     {
-        { Logic.Finished, new Color32(63, 63, 63, 255) },
-        { Logic.NoneReachable, new Color32(207, 16, 16, 255) },
-        { Logic.SomeReachable, new Color32(255, 159, 32, 255) },
-        { Logic.AllReachable, new Color32(32, 255, 32, 255) },
+        { Logic.Reachable, new Color32(32, 255, 32, 255) },
+        { Logic.UnReachable, new Color32(207, 16, 16, 255) },
+        { Logic.OutOfLogic, new Color32(255, 159, 32, 255) },
+        { Logic.Collected, new Color32(63, 63, 63, 255) },
     };
 }
--- BlasII.Randomizer.MapTracker/Enums/Logic.cs
+++ BlasII.Randomizer.MapTracker/Enums/Logic.cs
@@ -1,10 +1,25 @@
 ﻿
 namespace BlasII.Randomizer.MapTracker.Enums;
 
-internal enum Logic
+internal enum LogicOld
 {
     Finished,
     NoneReachable,
     SomeReachable,
     AllReachable,
+}
+
+[System.Flags]
+internal enum Logic
+{
+    Reachable = 0x01,
+    UnReachable = 0x02,
+    OutOfLogic = 0x04,
+    Collected = 0x08,
+
+    MixedReachableUnreachable = Reachable | UnReachable,
+    MixedReachableOutOfLogic = Reachable | OutOfLogic,
+    MixedUnReachableOutOfLogic = UnReachable | OutOfLogic,
+
+    MixedAll = Reachable | UnReachable | OutOfLogic,
 }

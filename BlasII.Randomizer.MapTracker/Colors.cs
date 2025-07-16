206f0d6376228c26872a38d0ad48eea21ab77e2e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-16 14:17:10 -0400

    core: remove comments

--- BlasII.Randomizer.MapTracker/Colors.cs
+++ BlasII.Randomizer.MapTracker/Colors.cs
@@ -1,19 +1,10 @@
-﻿using System.Collections.Generic;
-using BlasII.Randomizer.MapTracker.Enums;
+﻿using BlasII.Randomizer.MapTracker.Enums;
 using UnityEngine;
 
 namespace BlasII.Randomizer.MapTracker;
 
 internal static class Colors
 {
-    //public static Dictionary<Logic, Color> LogicColors { get; } = new()
-    //{
-    //    { Logic.Collected, new Color32(63, 63, 63, 255) },
-    //    { Logic.Reachable, new Color32(41, 175, 52, 255) },
-    //    { Logic.UnReachable, new Color32(179, 6, 0, 255) },
-    //    { Logic.OutOfLogic, new Color32(55, 96, 227, 255) },
-    //};
-
     public static Color Green { get; } = new Color32(41, 175, 52, 255);
 
     public static Color Blue { get; } = new Color32(55, 96, 227, 255);
